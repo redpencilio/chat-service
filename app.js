2058c4963509a0eee37ff1fbf9ccb125ac2812a4
Jan-Pieter Baert <jan-pieter@baert.jp.net>
2021-07-07 16:52:50 +0200

    Add message backend which will send basic push updates

---
+++ app.js
@@ -0,0 +1,57 @@
+import {
+    app,
+    uuid,
+    query,
+    sparqlEscape
+} from "mu";
+
+let messages = {}
+
+app.get('/messages/', async function(req, res) {
+    let id = req.get("MU-TAB-ID");
+    res.send({
+        data: messages[id] || []
+    })
+})
+
+app.post('/messages/', async function(req, res) {
+    let id = req.get("MU-TAB-ID");
+    let data = req.body.data;
+    let toId = data.attributes.to;
+    data.id = uuid()
+    data.attributes.from = id
+    data.attributes.time = new Date()
+    if (!messages[toId]) {
+        messages[toId] = []
+    }
+    messages[toId].push(data);
+    let test = []
+    res.send({
+        data: data
+    })
+
+    let uuidValue = uuid();
+    let value = sparqlEscape(JSON.stringify({
+        refresh: true
+    }), 'string')
+    let type = "http://chat"
+    let q = `
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX mupush: <http://mu.semte.ch/vocabularies/push/>
+    INSERT INTO <http://mu.semte.ch/application> {
+        <http://semte.baert.jp.net/push-updates/v0.1/${uuidValue}>  mu:uuid ${sparqlEscape(uuidValue, 'string')};
+                                                                    a mupush:PushUpdate;
+                                                                    mupush:tabId ${sparqlEscape(toId, 'string')};
+                                                                    mupush:type <${type}>;
+                                                                    rdf:value ${value}.
+    }
+    `
+    query(q)
+        .then(() => {
+            console.log(`Added push update for ${id}`)
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+})
