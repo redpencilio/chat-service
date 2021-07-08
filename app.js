d497172f675d5a8d0b450427cc39626c02c54891
Jan-Pieter Baert <jan-pieter@baert.jp.net>
2021-07-08 17:15:41 +0200

    Add creation date

--- app.js
+++ app.js
@@ -18,9 +18,18 @@ app.post('/messages/', async function(req, res) {
     let id = req.get("MU-TAB-ID");
     let data = req.body.data;
     let toId = data.attributes.to;
+    let now = new Date();
     data.id = uuid()
     data.attributes.from = id
-    data.attributes.time = new Date()
+    data.attributes.time = now.toLocaleDateString(undefined, {
+        timeZone: "Europe/Brussels",
+        dateStyle: 'full',
+        timeStyle: 'long',
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    })
     if (!messages[toId]) {
         messages[toId] = []
     }
@@ -34,17 +43,21 @@ app.post('/messages/', async function(req, res) {
     let value = sparqlEscape(JSON.stringify({
         refresh: true
     }), 'string')
+    let dateISOString = now.toISOString()
     let type = "http://chat"
     let q = `
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
     PREFIX mupush: <http://mu.semte.ch/vocabularies/push/>
+    PREFIX dc:  <http://purl.org/dc/terms/>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
     INSERT INTO <http://mu.semte.ch/application> {
         <http://semte.baert.jp.net/push-updates/v0.1/${uuidValue}>  mu:uuid ${sparqlEscape(uuidValue, 'string')};
                                                                     a mupush:PushUpdate;
                                                                     mupush:tabId ${sparqlEscape(toId, 'string')};
                                                                     mupush:type <${type}>;
-                                                                    rdf:value ${value}.
+                                                                    rdf:value ${value};
+                                                                    dc:created ${sparqlEscape(dateISOString, 'string')}^^xsd:dateTime.
     }
     `
     query(q)
